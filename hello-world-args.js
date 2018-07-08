28448bba89509189f3730a855142301a1d6b0081
Raquel Pau <raquelpau@gmail.com>
2018-07-08 11:52:13 +0200

    IO example

--- hello-world-args.js
+++ hello-world-args.js
@@ -1,4 +1,10 @@
 
-let args = process.argv;
-console.log(args);
-
+let readlineSync = require('readline-sync');
+ 
+ // Wait for user's response.
+ let userName = readlineSync.question('May I have your name? ');
+ console.log('Hi ' + userName + '!');
+  
+ // Handle the secret text (e.g. password).
+ let favFood = readlineSync.question('What is your favorite food? ');
+ console.log('Oh, ' + userName + ' loves ' + favFood + '!');
